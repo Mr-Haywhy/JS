ef07ec68b7f96baf5fb7a16690404d88c65e0e3c
Mr-Haywhy <tosinblessing.akinyemi@gamil.com>
2023-12-15 16:30:00 +0100

    adding some things more

--- additional_for_loop.js
+++ additional_for_loop.js
@@ -58,3 +58,145 @@ for(let x=0.2; x<3.0; x += 0.2)
                 break;
         }
 
+/**JavaScript will evaluate expression, pick the first case that matches, and then execute
+ * statements until it sees a break, return, continue, throw or the end of the switch
+ * statement */
+
+            switch (totalBet) {
+                case 7:
+                    totalBet = funds;
+                    break;
+                case 11:
+                    totalBet = 0;
+                    break;
+                case 13:
+                    totalBet = 0;
+                    break;
+                case 21:
+                    totalBet = 21;
+                    break;
+            }
+
+
+/**Note: There is something called "fall-through execution" In the switch statement above,
+ * thesame action is being taken when the bet is 11 or 13. A switch statement will keep 
+ * executing statements until it sees a break statement. This is were we might want to 
+ * take the advantage of fall-through execution
+ */
+
+// So the statement becomes 
+
+            switch (totalBet) {
+                case 7:
+                    totalBet = funds;
+                    break;
+                case 11:
+                case 13:
+                    totalBet = 0;
+                    break;
+                case 21:
+                    totalBet = 21;
+                    break;
+            }
+
+
+
+/* The break statement can be replaced with return statement if we are using the switch 
+ * statement inside a function
+ */
+
+
+            function adjustBet(totalBet, funds) {
+                switch (totalBet) {
+                    case 7:
+                        return funds;
+                    case 13:
+                        return 0;                
+                    default:
+                        return totalBet;
+                }
+            }
+
+
+
+// FOR...IN LOOP
+
+/**The for...in loop is designed to loop over the property keys of an object.
+ *          for(variable in object)
+ *              statement
+ * 
+ *          (const, let, var) 'OBJ-identifier' = {Object}
+ *           for(let 'property' in 'identifier'){
+ *              if(!'OBJ-identifier'.hasOwnProperty(var 'property')) 'built in function'continue;
+ *              console.log(property + string + OBJ-identifier[property]);
+ *          }
+ */
+
+            const player = {name: 'Thomas', rank: 'Midshipman', age: 25 };
+            for(let property in player) {
+                if(!player.hasOwnProperty(property)) continue;
+                console.log(property + ': ' + player[property]);
+            }
+
+
+// FOR...OF LOOP
+
+/**The for..of loop operator is another way to loop over the elements in a collection.
+ *          for(variable of object)
+ *              statement
+ * 
+ * The for...of loop can be used on arrays, but more generically, on any object that is
+ * iteratable. 
+ * 
+ *          (const, let, var) name = [array];
+ *          for(let (variable) of name)
+ *              console.log(`string template`);
+ */
+
+            const hand = [randFace(), randFace(), randFace()];
+            for(let face of hand)
+                console.log(`You rolled.....${face}!`);
+
+
+/**It is a great choice when you need to loop over an array, but don't need to
+ * know the index number of each element. If you need to know the indexes, use a regular
+ * for loop. 
+ */
+
+
+            const hand1 = [randFace(), randFace(), randFace()];
+            for(let i=0; i<hand1.length; i++)
+                console.log(`Roll ${i+1}: ${hand1[i]}`);
+
+
+
+
+// USEFUL CONTROL FLOW PATTERNS
+// Using continue to reduce conditional nesting
+/**Very often, in the body of a loop, you'll only want to continue to execute the body under
+ * certain circumstances (essentially combining a loop control flow with a conditional control
+ * flow).
+ */
+
+            while(funds > 1 && funds < 100) {
+                let totalBet = rand(1, funds);
+                if(totalBet === 13) {
+                    console.log("Unlucky! Skip this round.....");
+                } else {
+                    // play.....
+                }
+            }
+
+/**This is an example of 'nested control flow' inside the body of the while loop, the bulk
+ * of the action is inside the else clause; all we do inside the if clause is to call 
+ * console.log. We can use continue statements to flatten this structure.
+*/
+
+            while(funds > 1 && funds < 100) {
+                let totalBet = rand(1, funds);
+                if(totalBet === 13) {
+                    console.log("Unlucky! Skip this round.....");
+                    continue;
+                }
+                // play....
+            }
