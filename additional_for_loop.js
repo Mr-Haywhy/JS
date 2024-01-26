9753dafc73f068bbf843beb0fea571f16bbed1e1
Akinyemi Blessing Tosin <tosinblessing.akinyemi@gmail.com>
2024-01-26 16:18:51 +0100

    More add up

--- additional_for_loop.js
+++ additional_for_loop.js
@@ -268,3 +268,10 @@ for(let x=0.2; x<3.0; x += 0.2)
             for(let i=bigArrayOfNumbers.length-1; i >= 0; i--) {
                 if(isPrime(bigArrayOfNumbers[i])) bigArrayOfNumbers.splice(i, 1);
             }
+
+
+/**Control flow is really what makes our programs tick. Variables and constants may
+ * contain all the interesting information, but control flow statements allow us to make
+ * useful choices based on that data.
+ * 
+ */
